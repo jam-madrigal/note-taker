62ddc9f3d426be13dfa485365ca5ea35d1eb3bab
Alex Madrigal <jam.madrigal@gmail.com>
2020-08-27 16:49:04 -0700

    the html routes are established with express

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = function(app) {
+  // HTML GET Requests
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  // If no matching route is found default to index.html
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
--- server.js
+++ server.js
@@ -4,8 +4,25 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 
+// Tells node that we are creating an "express" server
+const app = express();
 
-const PORT = 8080;
+// Sets an initial port
+const PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//Setting up the location of the routes to be used
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
   
