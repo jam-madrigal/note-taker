b2dbf5179ffd4872f0aa8c3b791ff4d8707541e6
Alex Madrigal <jam.madrigal@gmail.com>
2020-08-29 12:25:22 -0700

    removed some comments/added some new comments, the app.delete is now linked up and ready to have a function written into it; it console.logs correctly on button click

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,22 +11,28 @@ module.exports = function(app) {
   });
 
   // API POST Requests
-  app.post("/api/notes", function(req, res) {
-    // If there is any data in newNote in the handleNoteSave function, push it into the request array, then write to db.json
+  app.post("/api/notes", (req, res) => {
+    // Define where the object containing the note is in the request
       const note = req.body;
-      // Assigning the notes a random id from 1-1000 and pushing it to the db.json
+      // Assigning the note object a random id from 1-1000 and pushing it to the db.json, which is an array of objects
+      // ***Figure out how to ensure no new note receives an existing id # in future versions***
       note.id = Math.ceil(Math.random() * 1000);
       notesData.push(note);
-      // return the current note, find out why this is not working and rendering notes on the page
+      // return the current note
       res.json(note);
 
-      // Writing the file
+      // Re-writing the db.json file with the newly pushed object
       fs.writeFileSync(
         path.join(__dirname, '../db/db.json'),
         JSON.stringify(notesData), null, 2)
 
     }
   );
+  // Create an app.delete, each note should have a unique url and this function will loop through the information from db.json to find the corresponding note for that url/id, remove it, then rewrite the db.json file
+  app.delete("/api/notes/:id", (req, res) => {
+    console.log("Delete request from the user.");
+
+  });
+
 };
 
-// Create an app.delete, each note should have a unique url and this function will loop through the information from db.json to find the corresponding note for that url/id, remove it, then rewrite the db.json file
